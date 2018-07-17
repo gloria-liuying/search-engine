9427e6f42c0e8470e673dd3601e46b8f5c7162c0
codegirlly <coderly2017@163.com>
2018-07-17 23:24:10 +0800

    文档写入数据库

--- code/index.py
+++ code/index.py
@@ -51,8 +51,36 @@ class IndexModule:
         except ValueError:
             return False
 
+    #过滤停用词
+    def clean_list(self,seg_list):
+        cleaned_dict = {}
+        n=0
+        for i in seg_list:
+            i=i.strip().lower()
+            if i !='' and not self.is__number(i)and i not in self.stop_words:
+                n = n+1
+                if i in cleaned_dict:
+                    cleaned_dict[i]=cleaned_dict[i]+1
+                else:
+                    cleaned_dict[i] = 1
+            return n,cleaned_dict
 
+            # 文档写入数据库
+            def write_postings_to_db(self, db_path):
+                conn = sqlite3.connect(db_path)
+                c = conn.cursor()
 
+                c.execute('''DROP TABLE IF EXISTS postings''')
+                c.execute('''CREATE TABLE postings
+                             (term TEXT PRIMARY KEY, df INTEGER, docs TEXT)''')
+
+                for key, value in self.postings_lists.items():
+                    doc_list = '\n'.join(map(str, value[1]))
+                    t = (key, value[0], doc_list)
+                    c.execute("INSERT INTO postings VALUES (?, ?, ?)", t)
+
+                conn.commit()
+                conn.close()
 
 
 if __name__ == "__main__":
